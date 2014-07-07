016e8d5064a3fccd33c9ab896b58f9543f1bfe72
hachi-eiji <hachiyae@gmail.com>
2014-07-08 01:30:18 +0900

    add install mysql

---
+++ script/install_mysql.sh
@@ -0,0 +1,8 @@
+curl -LOs http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-shared-compat-5.6.19-1.linux_glibc2.5.x86_64.rpm
+sudo yum -y install MySQL-shared-compat-5.6.19-1.linux_glibc2.5.x86_64.rpm
+curl -LOs http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.19-1.linux_glibc2.5.x86_64.rpm
+sudo yum -y install MySQL-client-5.6.19-1.linux_glibc2.5.x86_64.rpm
+curl -LOs http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.19-1.linux_glibc2.5.x86_64.rpm
+sudo yum -y install MySQL-server-5.6.19-1.linux_glibc2.5.x86_64.rpm
+
+exit 0
