c03c9b68b93e8b0a549aecb7372fe8a1b03ff166
Eiji Hachiya <hachi-eiji@users.noreply.github.com>
2014-07-19 22:38:07 +0900

    Update install_mysql.sh

--- script/install_mysql.sh
+++ script/install_mysql.sh
@@ -1,3 +1,5 @@
+#!bin/sh
+
 curl -LOs http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-shared-compat-5.6.19-1.linux_glibc2.5.x86_64.rpm
 sudo yum -y install MySQL-shared-compat-5.6.19-1.linux_glibc2.5.x86_64.rpm
 curl -LOs http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.19-1.linux_glibc2.5.x86_64.rpm
